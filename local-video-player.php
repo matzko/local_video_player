6f5180607b63bfb321feb3e7ca8a4e05d8026979
Austin Matzko <austin@pressedcode.com>
2011-03-28 13:29:57 -0500

    set playlist properties api

--- local-video-player.php
+++ local-video-player.php
@@ -13,6 +13,7 @@ if ( ! class_exists( 'LocalVideoPlayer' ) ) {
 		protected $_plugin_dir_url = '';
 		protected $_clip_values = array();
 		protected $_player_events = array();
+		protected $_playlist_properties = array();
 
 		public function __construct()
 		{
@@ -38,10 +39,26 @@ if ( ! class_exists( 'LocalVideoPlayer' ) ) {
 			$clip_values = apply_filters( 'local_video_player_clip_values', $this->_clip_values );
 			
 			$video_files = apply_filters( 'local_video_player_video_files', $video_files );
+			$playlist = array();
+			foreach( (array) $video_files as $playlist_item ) {
+				$item_play = array(
+					'url' => $playlist_item,
+				);
+				$props = apply_filters( 
+					'local_video_player_playlist_item_properties',
+					$this->_get_playlist_item_properties( $playlist_item ),
+					$playlist_item
+				);
+
+				if ( ! empty( $props ) ) {
+					$item_play = array_merge( $item_play, $props );	
+				}
+				$playlist[] = $item_play;
+			}
 
 			$player_events = apply_filters( 'local_video_player_player_events', array_merge(
 				array(
-					'playlist' => $video_files,
+					'playlist' => $playlist,
 				),
 				$this->_player_events
 			) );
@@ -56,6 +73,22 @@ if ( ! class_exists( 'LocalVideoPlayer' ) ) {
 			) );
 
 			return array( $config_string, $general_string );
+		}
+
+		protected function _get_playlist_item_properties( $url = '' )
+		{
+			$props = array();
+			if ( ! empty( $this->_playlist_properties[ '*' ] ) ) {
+				$props = $this->_playlist_properties[ '*' ];  
+			}
+
+			if ( ! empty( $this->_playlist_properties[ $url ] ) ) {
+				$props = array_merge(
+					$props,
+					$this->_playlist_properties[ $url ]
+				);
+			}
+			return $props;
 		}
 
 		protected function _literalize_strings( $text = '' )
@@ -83,7 +116,7 @@ if ( ! class_exists( 'LocalVideoPlayer' ) ) {
 				unset( $this->_clip_values[$key] );
 			} else {
 				if ( $is_js_literal ) {
-					$value = '%%RMS%' . $value . '%%RME%s';
+					$value = $this->make_js_literal( $value );
 				}
 				$this->_clip_values[$key] = $value;
 			}
@@ -102,10 +135,30 @@ if ( ! class_exists( 'LocalVideoPlayer' ) ) {
 				unset( $this->_player_events[$key] );
 			} else {
 				if ( $is_js_literal ) {
-					$value = '%%RMS%' . $value . '%%RME%s';
+					$value = $this->make_js_literal( $value );
 				}
 				$this->_player_events[$key] = $value;
 			}
+		}
+
+		public function add_playlist_property( $url = '', $key = '', $value = '' )
+		{
+			if ( empty( $this->_playlist_properties[ $url ] ) ) {
+				$this->_playlist_properties[ $url ] = array(); 
+			}
+			$this->_playlist_properties[ $url ][ $key ] = $value; 
+		}
+
+		/**
+		 * Wrap the text so that it gets rendered as a JS literal, i.e. not quoted 
+		 * when it gets JSON-encoded.
+		 *
+		 * @param string $text The text to make into a JS literal.
+		 * @return string The wrapped text.
+		 */
+		public function make_js_literal( $text = '' )
+		{
+			return '%%RMS%' . $text . '%%RME%s';
 		}
 
 		public function print_video_player($file_url = '', $id = '', $height = 330, $width = 520)
